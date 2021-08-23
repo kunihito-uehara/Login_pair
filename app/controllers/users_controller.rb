a987bdf312d0feedd862cd098b6ae7abec56fd78
kunihitouehara <uekuni2000@gmail.com>
2021-08-23 17:44:35 +0900

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      #flash.now[:danger] = 'ログインに失敗'
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy#ログアウトの処理
+    # session.delete(:user_id)
+    # flash[:notice] ='ログアウトしました'
+    # redirect_to new_session_path
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+    :password_confirmation)
+  end
+end
